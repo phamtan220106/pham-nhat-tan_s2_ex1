577f1c93ecdb407b359af7fa1aac352610694281
phamtan220106 <tanpham220106z@gmail.com>
2024-11-20 21:15:35 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int SoHocSinh;//kieu so nguyen
+    float DiemTB; //kieu so thuc
+    char TenSV; //kieu ki tu
+    double MaSV;//kieu so thuc nhung lon hon
+    printf("pham_nhat_tan_s1_ex1\n");
+    printf("Hello world!\n");
+    return 0;
+}
